f5646759a2e6d5eb36f60fd1000a884f49c2f15a
GaryHjy <782316959@qq.com>
2019-09-15 00:55:42 +0800

    feat: data数据处理
    data数据处理

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,4 +1,4 @@
-import { isData, isObject } from './util'
+import { isData, isPlainObject } from './util'
 
 function encode(val: string): string {
   return encodeURIComponent(val)
@@ -31,7 +31,7 @@ export function buildUrl(url: string, params?: any): string {
     values.forEach(val => {
       if (isData(val)) {
         val = val.toISOString()
-      } else if (isObject(val)) {
+      } else if (isPlainObject(val)) {
         val = JSON.stringify(val)
       }
       parts.push(`${encode(key)}=${encode(val)}`)
