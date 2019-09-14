125d9dd7edbee29633141d99f60f70270d9a4b94
GaryHjy <782316959@qq.com>
2019-09-14 23:05:49 +0800

    feat: 开发环境配置
    开发环境配置

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Methods =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Methods
+  data?: any
+  params?: any
+}
