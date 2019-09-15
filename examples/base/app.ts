e8f1692c3cc1bac403a7088072bc6a0f86e34f07
GaryHjy <782316959@qq.com>
2019-09-15 23:23:41 +0800

    feat: 请求promise化
    请求promise化

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -79,6 +79,28 @@ import axios from '../../src/index';
 //   data: arr
 // })
 
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json',
+//     'Accept': 'application/json, text/plain, */*'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+
 axios({
   method: 'post',
   url: '/base/post',
@@ -86,17 +108,18 @@ axios({
     a: 1,
     b: 2
   }
+}).then((res) => {
+  console.log(res)
 })
 
 axios({
   method: 'post',
   url: '/base/post',
-  headers: {
-    'content-type': 'application/json',
-    'Accept': 'application/json, text/plain, */*'
-  },
+  responseType: 'json',
   data: {
-    a: 1,
-    b: 2
+    a: 3,
+    b: 4
   }
+}).then((res) => {
+  console.log(res)
 })
